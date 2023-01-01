f65bd3ab8d4acf282e608e4928a494b20df6d024
Mateusz Majerczyk <mateuszmajerczyk@gmail.com>
2023-01-01 20:13:28 +0100

    Prepared template for movies list and created view for it

--- netflix_app/urls.py
+++ netflix_app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, ProfileList, ProfileCreate
+from .views import home, ProfileList, ProfileCreate, MovieList
 
 
 app_name = 'netflix_app'
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', home.as_view(), name='home'),
     path('profiles/', ProfileList.as_view(), name="profile-list"),
     path('profiles/create/', ProfileCreate.as_view(), name="profile-create"),
+    path('watch/<str:profile_id>/', MovieList.as_view(), name="movie-list"),
 ]
--- netflix_app/views.py
+++ netflix_app/views.py
@@ -3,7 +3,8 @@ from django.views import View
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from . forms import ProfileForm
-from . models import Profile
+from . models import Profile, Movie
+
 
 class home(View):
     def get(self, request, *args, **kwargs):
@@ -43,3 +44,21 @@ class ProfileCreate(View):
             'form':form
         }
         return render(request, 'profile-create.html', context)
+
+
+@method_decorator(login_required, name='dispatch')
+class MovieList(View):
+    def get(self, request, profile_id, *args, **kwargs):
+        try:
+            profile = Profile.objects.get(uuid=profile_id)
+            movies = Movie.objects.filter(age_limit=profile.age_limit)
+            if profile not in request.user.profiles.all():
+                return redirect('netflix_app:profile-list')
+
+            context = {
+            'movies':movies
+            }
+
+            return render(request, 'movie-list.html', context)
+        except Profile.DoesNotExist:
+            return redirect('netflix_app:profile-list')
