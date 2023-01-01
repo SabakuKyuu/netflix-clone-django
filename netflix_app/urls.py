a19426909b7aa02596771fee314fa31adee07afd
Mateusz Majerczyk <mateuszmajerczyk@gmail.com>
2023-01-01 19:35:31 +0100

    Created view and urls for profiles. Added profiles list for users

--- netflix_app/urls.py
+++ netflix_app/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import home, ProfileList
 
 
 app_name = 'netflix_app'
 
 urlpatterns = [
-    path('', home.as_view(), name='home')
+    path('', home.as_view(), name='home'),
+    path('profiles/', ProfileList.as_view(), name="profile-list"),
 ]
--- netflix_app/views.py
+++ netflix_app/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 class home(View):
     def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('netflix_app:profile-list')
         return render(request, 'index.html')
+
+
+@method_decorator(login_required, name='dispatch')
+class ProfileList(View):
+    def get(self, request, *args, **kwargs):
+        profiles = request.user.profiles.all()
+
+        context = {
+            'profiles':profiles
+        }
+        return render(request, 'profile-list.html', context)
